7a595947031b31e63de6413c7b4240fd731c5509
Rabby-XQ <www.rabbi0110110@gmail.com>
2025-12-14 22:04:51 +0600

    removed unused code

--- condition.js
+++ condition.js
@@ -2,12 +2,10 @@
 
 // a =5 and b= "5"
 function condition() {
-  const data = 2;
-  if (data % 2 === 0 || data === 0) {
-    console.log(`${data} is a even number`);
-  }
+    const data = 2;
+    if (data % 2 === 0 || data === 0) {
+        console.log(`${data} is a even number`);
+    }
 }
 
 condition();
-
-// 74 => A
